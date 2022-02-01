00e6da44860f8432e249f020b26ad849952d73cc
kumaranand1 <96856525+kumaranand1@users.noreply.github.com>
2022-02-01 12:57:24 +0530

    Delete library-rest-app/demo-library directory

--- library-rest-app/demo-library/src/main/java/com/dxc/practice/demolibrary/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dxc.practice.demolibrary.repository;
-import com.dxc.practice.demolibrary.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public class Authorrepository extends JpaRepository<Author,Integer> {
-    
-}
-
--- library-rest-app/demo-library/src/main/java/com/dxc/practice/demolibrary/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dxc.practice.demolibrary.repository;
-
-import com.dxc.practice.demolibrary.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class Bookrepository extends JpaRepository<Book ,Integer> {
-    
-}
